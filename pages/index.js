024b539e9a376908d0b7ef309bc99947fd160bbf
ronkips <kipshillash@gmail.com>
2023-01-31 17:19:53 +0300

    update the input and button

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,53 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import twitterLogo from "../assets/twitter.webp";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>My AI Avatar picture generator</h1>
           </div>
           <div className="header-subtitle">
             <h2>description of your generator</h2>
+          </div>
+          <div className="prompt-container">
+            <input
+              className="prompt-box"
+              value={input}
+              placeholder="type here"
+              onChange={handleChange}
+            />
+            <div className="prompt-buttons">
+              <a className="generate-button">
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-avatar"
+          href="https://github.com/ronkips/ai-avatar-starter.git"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={twitterLogo} alt="twitter logo" />
+            <p>my github repository</p>
           </div>
         </a>
       </div>
